4a60e8c3528c27bf41a457cbe4c553667fd11ec8
TakanashaTaryu <tatsuarieyu@gmail.com>
2024-11-26 01:37:38 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brownDask: '#6F4E37',
+        brownLight: '#A67B5B',
+        brownLight1: '#ECB176',
+        brownLight2: '#FED8B1',
+    },
+  },
+},
+  plugins: [],
+}
